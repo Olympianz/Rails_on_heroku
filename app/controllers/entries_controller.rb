344d1c8725135e4385b1b3bb09d519aef954b24c
olympianz <yue_wu_di@hotmail.com>
2013-04-26 03:00:47 -0400

    small styling

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,7 +5,7 @@ class EntriesController < ApplicationController
   respond_to :html
 
   before_filter :get_entries
-  before_filter :find_entry,     only: [:show, :edit, :update]
+  before_filter :find_entry,     only: [:show, :edit, :update,:destroy]
   before_filter :build_children, only: [:new, :edit]
   
   def index
@@ -41,7 +41,7 @@ class EntriesController < ApplicationController
   end
   
   def destroy
-    Entry.find(params[:id]).destroy
+    @entry.destroy
     flash_notice('deleted')
     redirect_to entries_url
   end
